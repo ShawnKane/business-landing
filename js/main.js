2a3ca9b5f795d4270bea0cb60d410528a67e3e99
ShawnKane <88581764+ShawnKane@users.noreply.github.com>
2021-08-31 16:44:07 +0300

    upload

---
+++ js/main.js
@@ -0,0 +1,10 @@
+// $(window).scroll(function() {
+// 	$('#awaits-3').each(function(){
+// 	    var imagePos = $(this).offset().top;
+
+// 	    var topOfWindow = $(window).scrollTop();
+// 	    if (imagePos < topOfWindow+650) {
+// 	        $(this).addClass("fadeInRight");
+// 	        }
+// 	    });
+// 	})
